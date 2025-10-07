7f5a5812c67f30af9a86bbba504e3867e39a471f
oktetod <108335533+oktetod@users.noreply.github.com>
2025-10-07 17:23:14 +0800

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -80,7 +80,7 @@ def download_model():
     image=image,
     gpu="T4",
     volumes={MODEL_DIR: model_volume},
-    container_idle_timeout=300
+    container_idle_timeout=20
 )
 class ModelInference:
     @modal.enter()
